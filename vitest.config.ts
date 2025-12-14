58f6f0535c8d0e90099db74eba16f778275e6837
zojize <jeffzo01@qq.com>
2025-12-14 00:15:05 -0600

    paper

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vitest/config"
+// @ts-ignore: shouldn't error
+import Plugin from "./src/index.js"
 
 export default defineConfig({
+  plugins: [Plugin()],
   test: {
     include: ["./test/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    projects: [
+      // root is included here to ensure the plugin is executed
+      ".",
+      // "sample-projects/*"
+    ],
     exclude: [],
     globals: true,
     coverage: {
